74a1da543573db8910a0a9f1722366dc7dda0cb7
ainacs <aina.caselles@goumh.umh.es>
2023-05-29 13:28:51 +0200

    AñadO canvas y SVG

--- JS/main.js
+++ JS/main.js
@@ -1,61 +1,87 @@
+var posX_izq = 66; //pupila izq
+var posX_der = 154; //pupila der
+var posY = 70;
 
-//Elemento CANVAS
-function draw() {
-    // Obtenemos el elemento del lienzo 
-    var canvas = document.getElementById("personaje");
-    // Obtenemos el contexto 2D del lienzo
-    var ctx = canvas.getContext("2d");
+var canvas, ctx;
 
+function rana(){
+    canvas = document.getElementById('personaje');
+    ctx = canvas.getContext('2d');
+    
+    // Cabeza
+    //ctx.beginPath();
+    ctx.arc(110, 110, 80, 0, 2 * Math.PI); // Cuerpo
+    ctx.fillStyle = "#1CE500"; // Establecemos el color a pintar: verde claro
+    ctx.fill(); // Pintamos el circulo 
 
-// Cabeza
-ctx.beginPath();
-ctx.arc(100, 100, 80, 0, 2 * Math.PI); // Cuerpo
-ctx.fillStyle = "#1CE500"; // Color verde claro
-ctx.fill();
-ctx.lineWidth = 2;
-ctx.strokeStyle = "black";
+    // OREJAS
+    // Izquierda
+    //ctx.beginPath();
+    ctx.arc(60, 60, 41, 0, 2 * Math.PI); // Forma
+    ctx.fillStyle = "#1CE500";
+    ctx.fill();
+    //Derecha
+    ctx.beginPath();
+    ctx.arc(160, 60, 41, 0, 2 * Math.PI); // Forma
+    ctx.fillStyle = "#1CE500";
+    ctx.fill();
 
-// OREJAS
-// Izquierda
-ctx.beginPath();
-ctx.arc(50, 50, 41, 0, 2 * Math.PI); // Forma
-ctx.fillStyle = "#1CE500";
-ctx.fill();
-//Derecha
-ctx.beginPath();
-ctx.arc(150, 50, 41, 0, 2 * Math.PI); // Forma
-ctx.fillStyle = "#1CE500";
-ctx.fill();
+    // Contorno de los ojos de la rana
+    ctx.beginPath();
+    ctx.arc(65, 64, 27, 0, 2 * Math.PI); // Ojo izquierdo
+    ctx.fillStyle = "white";
+    ctx.fill();
 
+    ctx.beginPath();
+    ctx.arc(155, 64, 27, 0, 2 * Math.PI); // Ojo derecho
+    ctx.fillStyle = "white";
+    ctx.fill();
 
-// Contorno de los ojos de la rana
-ctx.beginPath();
-ctx.arc(55, 54, 27, 0, 2 * Math.PI); // Ojo izquierdo
-ctx.fillStyle = "white";
-ctx.fill();
+    // Pupilas de los ojos
+    //function pupilas(){
+    ctx.beginPath();
+    ctx.arc(posX_izq, posY, 18, 0, 2 * Math.PI); // Ojo izquierdo
+    ctx.fillStyle = "black";
+    ctx.fill();
 
-ctx.beginPath();
-ctx.arc(145, 54, 27, 0, 2 * Math.PI); // Ojo derecho
-ctx.fillStyle = "white";
-ctx.fill();
+    ctx.beginPath();
+    ctx.arc(posX_der, posY, 18, 0, 2 * Math.PI); // Ojo derecho
+    ctx.fillStyle = "black";
+    ctx.fill();
 
-// Pupilas de los ojos
-ctx.beginPath();
-ctx.arc(56, 60, 18, 0, 2 * Math.PI); // Ojo izquierdo
-ctx.fillStyle = "black";
-ctx.fill();
+    // Boca de la rana
+    ctx.beginPath();
+    ctx.ellipse(110, 150, 45, 30, 0, 0, 10 ); // boca
+    ctx.fillStyle ="red"; //color del elemento
+    ctx.fill(); //colorear
 
-ctx.beginPath();
-ctx.arc(144, 60, 18, 0, 2 * Math.PI); // Ojo derecho
-ctx.fillStyle = "black";
-ctx.fill();
+    window.requestAnimationFrame(rana);
+  }
+  
+  window.requestAnimationFrame(rana);
+  
+  // Funcion que pone en movimiento nuestro dibujo
+  var id;
+  function mover() {
+    id = setInterval(movimiento, 100); //establecemos la velocidad a la cual girará nuestro elemento
+    function movimiento() {
+        ctx.clearRect(0,0,225,225);
+        ctx.translate(110, 110); //movemos el punto sobre el cual girara la figura
+        ctx.rotate(Math.PI/30); // rotamos la figura en un angulo de 30º
+        ctx.translate(-110, -110); //volvemos a su posicion inicial
+    }
+  }
 
-// Boca de la rana
-ctx.beginPath();
-ctx.ellipse(100, 140, 45, 30, 0, 0, 10 ); // boca
-ctx.fillStyle ="red"; //color del elemento
-ctx.fill(); //colorear
-
-}
-
-window.onload = draw;
+  //Funcion para controlar si el boton se pulsa o no: va alternando la funcionalidad cada vez que se pulsa
+  var isToggleOn = false;
+  function handleClick() {
+    if (isToggleOn) { //Pulsamos de nuevo 1
+      clearInterval(id); //Detenemos la accion
+      isToggleOn = false; //Reiniciamos el boton
+      
+    } else { // Si pulsamos el boton por primera vez
+      mover(); // Iniciamos la accion 
+      isToggleOn = true; //Cambiamos el estado del boton para que la siguiente vez entre en el otro bucle
+    }
+  }
+    
